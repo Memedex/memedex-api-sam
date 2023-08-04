6f21fe9b734b6a368a2741aaaaaf0dbcb8f92aa1
stephenspol <stephenssspol@gmail.com>
2023-08-04 01:18:33 -0400

    Converted put-item lambda to java

---
+++ MemedexApi/src/main/java/net/ddns/memedex/handler/PutItem.java
@@ -0,0 +1,65 @@
+package net.ddns.memedex.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.extern.slf4j.Slf4j;
+import net.ddns.memedex.model.Item;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+
+@Slf4j
+public class PutItem implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    protected String tableName = System.getenv("SAMPLE_TABLE");
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        if (!event.getHttpMethod().equals("POST")) {
+            throw new RuntimeException("PutItem only accept POST method, you tried: " + event.getHttpMethod());
+        }
+
+        log.debug("received: {}", event);
+
+        Gson gson = new GsonBuilder().create();
+        Item item = gson.fromJson(event.getBody(), Item.class);
+
+        DefaultCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+        DynamoDbClient ddb = DynamoDbClient.builder()
+                .region(Region.US_EAST_2)
+                .credentialsProvider(credentialsProvider)
+                .build();
+
+        DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(ddb)
+                .build();
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+
+        try {
+            this.putItem(enhancedClient, item);
+
+            response.withStatusCode(200);
+        } catch (DynamoDbException e) {
+            log.error("DynamoDb exception occurred!", e);
+            response.withStatusCode(400).withBody(e.getMessage());
+        }
+
+        log.debug("response from: {} statusCode: {} body: {}", event.getPath(), response.getStatusCode(), response.getBody());
+        return response;
+    }
+
+    protected void putItem(DynamoDbEnhancedClient enhancedClient, Item item) throws DynamoDbException {
+        DynamoDbTable<Item> sampleTable = enhancedClient.table(tableName, TableSchema.fromBean(Item.class));
+
+        // Put the item into an Amazon DynamoDB table.
+        sampleTable.putItem(item);
+    }
+}
